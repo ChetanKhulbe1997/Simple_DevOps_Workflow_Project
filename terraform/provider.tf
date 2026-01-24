74b4a34e984e25da682039f477eee2a84a5b86c4
ChetanKhulbe1997 <chetankhulbe2016@gmail.com>
2026-01-24 13:54:52 +0530

    Clean repo without terraform artifacts

---
+++ terraform/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "tf-state-bucket-for-my-project"
+    key    = "Simple_DevOps_Workflow_Project/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+
+provider "aws" {
+  region = "us-east-1"
+}
