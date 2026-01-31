c21be3ef752009b6333b004fd3c5644f1c989bbd
ChetanKhulbe1997 <chetankhulbe2016@gmail.com>
2026-01-31 18:10:24 +0530

    Updated app.js

--- app/app.js
+++ app/app.js
@@ -10,8 +10,8 @@ app.get('/', (req, res) => {
         <style>
           body { font-family: sans-serif; display: flex; align-items: center; justify-content: center; height: 100vh; background: #f0f2f5; }
           .card { background: white; padding: 2rem; border-radius: 8px; box-shadow: 0 4px 6px rgba(241, 10, 118, 0.97); text-align: center; }
-          h1 { color: #e8e11a; }
-          p { color: #3ed071; }
+          h1 { color: #1ae1e8; }
+          p { color: #f50909; }
         </style>
       </head>
       <body>
