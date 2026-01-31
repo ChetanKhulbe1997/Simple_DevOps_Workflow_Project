8f5257ed2ce1298c4690720f423f2fc2a78d2440
ChetanKhulbe1997 <chetankhulbe2016@gmail.com>
2026-01-31 17:55:07 +0530

    Updated app.js

--- app/app.js
+++ app/app.js
@@ -9,17 +9,17 @@ app.get('/', (req, res) => {
         <title>DevOps Demo</title>
         <style>
           body { font-family: sans-serif; display: flex; align-items: center; justify-content: center; height: 100vh; background: #f0f2f5; }
-          .card { background: white; padding: 2rem; border-radius: 8px; box-shadow: 0 4px 6px rgba(0,0,0,0.1); text-align: center; }
+          .card { background: white; padding: 2rem; border-radius: 8px; box-shadow: 0 4px 6px rgba(241, 10, 118, 0.97); text-align: center; }
           h1 { color: #e8e11a; }
           p { color: #3ed071; }
         </style>
       </head>
       <body>
         <div class="card">
-          <h1>DevOps Workflow Demo by Chetan Khulbe</h1>
+          <h1>DevOps Workflow Demo Project by Chetan Khulbe</h1>
           <p>This is my first DevOps project!</p>
           <hr>
-          <p>Status: <strong>Running on EC2</strong></p>
+          <p>Status: <strong>Running on DevOps Server</strong></p>
         </div>
       </body>
     </html>
